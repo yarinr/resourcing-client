5eb8b393cd7a324484e7a81b198b6343e44f0eee
Yarin Ronel <y3arin@gmail.com>
2019-12-31 11:49:36 +0200

    added html&css, no logic yet

---
+++ src/app/shared/components/tutorial-card/tutorial-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tutorial-card',
+  templateUrl: './tutorial-card.component.html',
+  styleUrls: ['./tutorial-card.component.less']
+})
+export class TutorialCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { MaterialModule } from './material/material.module';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { TopicGridComponent } from './components/topic-grid/topic-grid.component';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MaterialModule } from "./material/material.module";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
+import { TutorialCardComponent } from "./components/tutorial-card/tutorial-card.component";
+import { TopicGridComponent } from "./components/topic-grid/topic-grid.component";
 
 @NgModule({
-  declarations: [TopicGridComponent],
+  declarations: [TutorialCardComponent, TopicGridComponent],
   imports: [CommonModule, MaterialModule, ReactiveFormsModule, FormsModule],
   exports: [
     MaterialModule,
     ReactiveFormsModule,
     FormsModule,
-    TopicGridComponent
+    TopicGridComponent,
+    TutorialCardComponent
   ]
 })
 export class SharedModule {}
